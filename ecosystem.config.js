b5a354e6884972d5d82f35f71c95cf912bbfd0f8
Cardanoian <superiorz1@yu.ac.kr>
2025-10-11 18:43:13 +0900

    fix: Add environment file configuration and adjust restart settings in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,18 +7,19 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      env_file: '.env',
       env: {
         NODE_ENV: 'development',
-        PORT: 3050,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3050,
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
       log_file: './logs/combined.log',
       time: true,
+      max_restarts: 10, // 최대 재시작 횟수 제한
+      min_uptime: '10s', // 최소 실행 시간
     },
   ],
 };
